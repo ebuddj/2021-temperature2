9eb2d1e3048a2c33c37af16cba7fc98c4f14cb16
Teemo Tebest <teemo.tebest@gmail.com>
2021-07-26 11:36:52 +0200

    Initial commit

---
+++ src/jsx/App.jsx
@@ -0,0 +1,33 @@
+import React, {Component} from 'react';
+import style from './../styles/styles.less';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+    }
+  }
+  componentDidMount() {
+
+  }
+  componentDidUpdate(prevProps, prevState, snapshot) {
+
+  }
+  componentWillUnMount() {
+
+  }
+  // shouldComponentUpdate(nextProps, nextState) {}
+  // static getDerivedStateFromProps(props, state) {}
+  // getSnapshotBeforeUpdate(prevProps, prevState) {}
+  // static getDerivedStateFromError(error) {}
+  // componentDidCatch() {}
+  render() {
+    return (
+      <div className={style.app}>
+        Build your application
+      </div>
+    );
+  }
+}
+export default App;
