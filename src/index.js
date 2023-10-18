ff76a0a92f2c1c54381f049eb35b505b7e631378
Teemo Tebest <teemo.tebest@gmail.com>
2023-10-18 15:26:32 +0200

    Updated id name

--- src/index.js
+++ src/index.js
@@ -4,6 +4,6 @@ import { createRoot } from 'react-dom/client';
 
 import App from './jsx/App.jsx';
 
-const container = document.getElementById('ebu-app-root');
+const container = document.getElementById('app-root-2021-temperature2');
 const root = createRoot(container);
 root.render(<App />);
