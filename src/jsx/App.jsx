a961835ed34d4f929849942247641e792c94b776
Teemo Tebest <teemo.tebest@gmail.com>
2023-10-18 15:12:34 +0200

    Modernised code

--- src/jsx/App.jsx
+++ src/jsx/App.jsx
@@ -1,153 +1,88 @@
-import React, {Component} from 'react';
-import style from './../styles/styles.less';
+import React, {
+  useState, useEffect, useCallback, useRef
+} from 'react';
 
 // https://d3js.org/
 import * as d3 from 'd3';
 
 // https://vis4.net/chromajs/
 import chroma from 'chroma-js';
+import '../styles/styles.less';
 // Use chroma to make the color scale.
 // https://gka.github.io/chroma.js/
-const scaleMax = 1,
-      scaleMin = -1,
-      f = chroma.scale('RdYlBu').domain([scaleMax, 0, scaleMin]),
-      f_text = chroma.scale(['red', 'rgba(0, 0, 0, 0.3)', 'blue']).padding(-1).domain([scaleMax, 0, scaleMin]);
-const margin = {top: 40, right: 40, bottom: 40, left: 40},
-      width = window.innerWidth - margin.left - margin.right,
-      height = window.innerHeight - margin.top - margin.bottom;
+const scaleMax = 1;
+const scaleMin = -1;
+const f = chroma.scale('RdYlBu').domain([scaleMax, 0, scaleMin]);
+const f_text = chroma.scale(['red', 'rgba(0, 0, 0, 0.3)', 'blue']).padding(-1).domain([scaleMax, 0, scaleMin]);
+const margin = {
+  top: 40, right: 40, bottom: 40, left: 40
+};
+const width = window.innerWidth - margin.left - margin.right;
+const height = window.innerHeight - margin.top - margin.bottom;
 
 const x = d3.scaleBand()
   .range([0, width]);
 const y = d3.scaleLinear()
   .range([0, height]);
 
-let interval,
-    chart_elements,
-    yAxis,
-    xAxis;
-
-let start_year = 1880;
+let chart_elements;
+let yAxis;
+let xAxis;
 
 const getHashValue = (key) => {
-  let matches = window.location.hash.match(new RegExp(key+'=([^&]*)'));
+  const matches = window.location.hash.match(new RegExp(`${key}=([^&]*)`));
   return matches ? matches[1] : null;
-}
+};
 
 const hemisphere = getHashValue('hemisphere') ? getHashValue('hemisphere').replace('%20', ' ') : 'World';
 const month = getHashValue('month') ? getHashValue('month') : 'Year';
 
 const hemispheres = {
-  'World':0,
-  'Northern hemisphere':1,
-  'Southern hemisphere':2
+  World: 0,
+  'Northern hemisphere': 1,
+  'Southern hemisphere': 2
 };
 const months = {
-  'January':'Jan',
-  'February':'Feb',
-  'March':'Mar',
-  'April':'Apr',
-  'May':'May',
-  'June':'Jun',
-  'July':'Jul',
-  'August':'Aug',
-  'September':'Sep',
-  'October':'Oct',
-  'November':'Nov',
-  'December':'Dec',
-  'Year':'J-D'
+  January: 'Jan',
+  February: 'Feb',
+  March: 'Mar',
+  April: 'Apr',
+  May: 'May',
+  June: 'Jun',
+  July: 'Jul',
+  August: 'Aug',
+  September: 'Sep',
+  October: 'Oct',
+  November: 'Nov',
+  December: 'Dec',
+  Year: 'J-D'
 };
 
-class App extends Component {
-  constructor(props) {
-    super(props);
+let start_year = 1880;
+let end_year = 2021;
 
-    this.state = {
-      current_temp:0
-    };
-  }
-  componentDidMount() {
-    const svg = d3.select('.' + style.chart_container)
-      .append('svg')
-      .attr('width', width + margin.left + margin.right)
-      .attr('height', height + margin.top + margin.bottom);
-    yAxis = svg.append('g')
-      .attr('class', style.yaxis)
-      .attr('transform', 'translate(' + (margin.left - 1) + ', ' + margin.top + ')');
-    xAxis = svg.append('g')
-      .attr('class', style.xaxis)
-      .attr('transform', 'translate(' + margin.left + ',' + (height - 50) + ')');
-    chart_elements = svg.append('g')
-      .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
+function App() {
+  const interval = useRef(null);
+  const [currentTemp, setCurrentTemp] = useState(false);
+  const [curYear, setCurYear] = useState(start_year);
+  const [data, setData] = useState(null);
 
-    this.getData();
-  }
-  componentDidUpdate(prevProps, prevState, snapshot) {
-
-  }
-  componentWillUnMount() {
-    clearInterval(interval);
-  }
-  getData() {
-    Promise.all([
-      d3.text('./data/GLB.Ts+dSST.csv'),
-      d3.text('./data/NH.Ts+dSST.csv'),
-      d3.text('./data/SH.Ts+dSST.csv')
-    ]).then((files) => {
-      files = files.map(file => d3.csvParse(file.split('\n').slice(1).join('\n')));
-      let start_year = 1880;
-      let end_year = 2021;
-      this.data = files[hemispheres[hemisphere]].map((file, i) => {
-        if (i === 0 && file[months[month]] === '***') {
-          file[month] === '***';
-          start_year = 1881;
-        }
-        else if (i === (files[hemispheres[hemisphere]].length - 1) && file[months[month]] === '***') {
-          end_year = 2020;
-        }
-        return +file[months[month]]
-      });
-      this.setState((state, props) => ({
-        end_year:end_year,
-        start_year:start_year,
-        year:start_year
-      }), () => this.setInterval());
-    }).catch((err) => {
-    });
-  }
-  setInterval() {
-    interval = setInterval(() => {
-      if (this.state.year >= this.state.end_year) {
-        this.setState((state, props) => ({
-          current_temp:this.data[this.state.year - start_year]
-        }))
-        clearInterval(interval);
-      }
-      else {
-        this.setState((state, props) => ({
-          current_temp:this.data[this.state.year - start_year],
-          year:state.year + 1
-        }), () => this.updateData());
-      }
-    }, 50);
-  }
-  updateData() { // https://www.d3-graph-gallery.com/graph/barplot_button_data_hard.html
-    let data = this.data.slice(0, this.state.year - start_year + 1);
-    x.domain(data.map((data, i) => i));
+  const updateData = useCallback(() => { // https://www.d3-graph-gallery.com/graph/barplot_button_data_hard.html
+    const bar_data = data.slice(0, curYear - start_year + 1);
+    x.domain(bar_data.map((el, i) => i));
     xAxis.call(d3.axisBottom(x));
 
-    y.domain([Math.max(0.15, d3.max(data, d => d)), Math.min(-0.3, d3.min(data, d => d))]);
+    y.domain([Math.max(0.15, d3.max(bar_data, d => d)), Math.min(-0.3, d3.min(bar_data, d => d))]);
     yAxis.call(d3.axisLeft(y)
       .ticks(0.5)
-      .tickFormat(i => {
-        return i + '°C'
-      })
+      .tickFormat(i => `${i}°C`)
       .tickSizeInner(-width)
       .tickSizeOuter(0));
 
-    let bars = chart_elements.selectAll('.bar')
-      .data(data);
+    const bars = chart_elements.selectAll('.bar')
+      .data(bar_data);
 
-    bars.exit().remove()
+    bars.exit().remove();
 
     bars.enter().append('rect')
       .attr('class', 'bar')
@@ -159,34 +94,116 @@ class App extends Component {
       .merge(bars)
       .attr('width', x.bandwidth())
       .attr('height', d => Math.abs(y(d) - y(0)))
-      .attr('y', d => (d > 0) ? y(Math.max(0, d)) : y(Math.max(0, d)) + 1)
+      .attr('y', d => ((d > 0) ? y(Math.max(0, d)) : y(Math.max(0, d)) + 1))
       .attr('x', (d, i) => x(i));
-  }
-  // shouldComponentUpdate(nextProps, nextState) {}
-  // static getDerivedStateFromProps(props, state) {}
-  // getSnapshotBeforeUpdate(prevProps, prevState) {}
-  // static getDerivedStateFromError(error) {}
-  // componentDidCatch() {}
-  render() {
-    return (
-      <div className={style.app}>
-        <div className={style.title_container}>
-          <h3>Temperature anomalies {(month !== 'Year') ? 'in ' + month : ''}</h3>
-          <h3>{this.state.start_year}–{this.state.end_year}</h3>
+  }, [curYear, data]);
+
+  const startInterval = useCallback(() => {
+    interval.current = setInterval(() => {
+      setCurYear(currentState => {
+        setCurrentTemp(data[currentState - start_year]);
+        const newState = currentState + 1;
+        if (newState >= end_year) {
+          clearInterval(interval.current);
+        }
+        return newState;
+      });
+    }, 50);
+
+    // Clearing the interval
+    return () => {
+      interval.current = null;
+      clearInterval(interval.current);
+    };
+  }, [data]);
+
+  const getData = useCallback(() => {
+    Promise.all([
+      d3.text(`${(window.location.href.includes('localhost:80')) ? './' : 'https://ebuddj.github.io/2021-temperature2/'}data/GLB.Ts+dSST.csv`),
+      d3.text(`${(window.location.href.includes('localhost:80')) ? './' : 'https://ebuddj.github.io/2021-temperature2/'}data/NH.Ts+dSST.csv`),
+      d3.text(`${(window.location.href.includes('localhost:80')) ? './' : 'https://ebuddj.github.io/2021-temperature2/'}data/SH.Ts+dSST.csv`)
+    ]).then((files) => {
+      files = files.map(file => d3.csvParse(file.split('\n').slice(1).join('\n')));
+
+      setData(files[hemispheres[hemisphere]].map((file, i) => {
+        if (i === 0 && file[months[month]] === '***') {
+          start_year = 1881;
+        } else if (i === (files[hemispheres[hemisphere]].length - 1) && file[months[month]] === '***') {
+          end_year = 2020;
+        }
+        return +file[months[month]];
+      }));
+    }).catch((err) => {
+      console.log(err);
+    });
+  }, []);
+
+  useEffect(() => {
+    if (interval.current === null && (data !== null)) {
+      startInterval();
+    }
+  }, [data, startInterval]);
+
+  useEffect(() => {
+    if (data !== null) {
+      updateData();
+    }
+  }, [data, curYear, updateData]);
+
+  useEffect(() => {
+    const svg = d3.select('.chart_container')
+      .append('svg')
+      .attr('width', width + margin.left + margin.right)
+      .attr('height', height + margin.top + margin.bottom);
+    yAxis = svg.append('g')
+      .attr('class', 'yaxis')
+      .attr('transform', `translate(${margin.left - 1}, ${margin.top})`);
+    xAxis = svg.append('g')
+      .attr('class', 'xaxis')
+      .attr('transform', `translate(${margin.left},${height - 50})`);
+    chart_elements = svg.append('g')
+      .attr('transform', `translate(${margin.left},${margin.top})`);
+
+    getData();
+  }, [getData]);
+
+  return (
+    <div className="app">
+      <div className="title_container">
+        <h3>
+          Temperature anomalies
+          {(month !== 'Year') ? `in ${month}` : ''}
+        </h3>
+        <h3>
+          {start_year}
+          –
+          {end_year}
+        </h3>
+      </div>
+      <div className="chart_container" />
+      <div className="info_container">
+        <div className="hemispehere">{hemisphere}</div>
+        <div className="year_container">{curYear}</div>
+        <div className="temp_container" style={{ color: f_text(currentTemp) }}>
+          {((currentTemp > 0) ? '+' : '') + currentTemp}
+          °C
         </div>
-        <div className={style.chart_container}></div>
-        <div className={style.info_container}>
-          <div className={style.hemispehere}>{hemisphere}</div>
-          <div className={style.year_container}>{this.state.year}</div>
-          <div className={style.temp_container} style={{color: f_text(this.state.current_temp)}}>{((this.state.current_temp > 0) ? '+': '') + this.state.current_temp}°C</div>
-          <div className={style.meta_container}>
-            <div>Data: <a href="https://data.giss.nasa.gov/gistemp/">NASA</a></div>
-            <div>Author: <a href="https://twitter.com/teelmo">Teemo Tebest</a>, EBU</div>
-            <div>Reference period: 1951–1980</div>
+        <div className="meta_container">
+          <div>
+            Data:
+            {' '}
+            <a href="https://data.giss.nasa.gov/gistemp/">NASA</a>
           </div>
+          <div>
+            Author:
+            {' '}
+            <a href="https://twitter.com/teelmo">Teemo Tebest</a>
+            , EBU
+          </div>
+          <div>Reference period: 1951–1980</div>
         </div>
       </div>
-    );
-  }
+    </div>
+  );
 }
 export default App;
